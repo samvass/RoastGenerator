eca0d1523d72ba1be4c39e961e985f5f8bef5854
Adam Geenen <adamgeenen@gmail.com>
2023-09-30 18:19:05 -0400

    Saves picture in backend

--- backend/llm/generateRoast.py
+++ backend/llm/generateRoast.py
@@ -1,11 +1,43 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 import openai
+import base64
+import re
+from PIL import Image
+from io import BytesIO
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+CORS(app)
+
+def data_url_to_image(data_url):
+    # Parse the data URL using regular expressions
+    match = re.match(r"data:(?P<type>.*?);base64,(?P<data>.*)", data_url)
+    if match:
+        image_type = match.group("type")
+        image_data_base64 = match.group("data")
+
+        # Decode the base64 image data
+        image_data_binary = base64.b64decode(image_data_base64)
+
+        # Create a PIL Image object from binary data
+        image = Image.open(BytesIO(image_data_binary))
+
+        save_path = "images/captured_image.jpg"
+        image.save(save_path)
+
+        return image
+
+    else:
+        raise ValueError("Invalid data URL format")
 
 @app.route('/')
 def test():
@@ -28,5 +60,42 @@ def generate_text():
     # Return the response from the language model to the React app
     return response.choices[0].message.content
 
+@app.route('/api/upload')
+def work():
+    return "Hello"
+
+@app.route('/api/upload', methods=['POST'])
+def upload_image():
+    print("WORK")
+    data = request.json['image']
+    data_url_to_image(data)
+
+    return "worked"
+
+    # # Check if a file was provided in the request
+    # if 'image' not in request.files:
+    #     print("NOTWORK")
+    #     return jsonify({'error': 'No image provided'}), 400
+
+    # print("WORK2")
+    # image = request.files['image']
+
+    # # Check if the file has an allowed extension
+    # if image and allowed_file(image.filename):
+    #     print("WORK3")
+    #     # Generate a unique filename for the image
+    #     filename = str(uuid.uuid4()) + '.jpg'
+    #     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    #     # Save the image to the uploads folder
+    #     image.save(filepath)
+
+    #     # Return a success response with the filename or URL
+    #     return jsonify({'message': 'Image uploaded successfully', 'filename': filename}), 200
+    # else:
+    #     return jsonify({'error': 'Invalid file format'}), 400
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
--- roastgen/src/webcam/Webcam.js
+++ roastgen/src/webcam/Webcam.js
@@ -1,40 +1,26 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import Webcam from 'react-webcam';
-import html2canvas from 'html2canvas';
-import download from 'downloadjs';
-import { BsCamera } from 'react-icons/bs'; 
+import { BsCamera } from 'react-icons/bs';
+import axios from 'axios'; // Import axios
 import "./Webcam.css"
 
 import faceOutline from './overlay.png'; // Import the outline image
 
 const WebcamCapture = () => {
   const webcamRef = useRef(null);
-  const [capturedImage, setCapturedImage] = useState(null);
 
   const capture = async () => {
     const imageSrc = webcamRef.current.getScreenshot();
-    setCapturedImage(imageSrc);
-  };
 
-  const saveImage = () => {
-    // if (capturedImage) {
-    //   html2canvas(document.querySelector('#capture')).then((canvas) => {
-    //     const ctx = canvas.getContext('2d');
-    //     const image = new Image();
-    //     image.src = capturedImage;
+    try {
+      // Send the captured image to the backend
+      console.log("WORK??")
+      const response = await axios.post('http://127.0.0.1:5000/api/upload', { image: imageSrc });
+      console.log('Image uploaded successfully:', response.data);
 
-    //     // Draw the captured image on the canvas
-    //     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-    //     // Draw the outline on top of the captured image
-    //     const outline = new Image();
-    //     outline.src = faceOutline;
-    //     ctx.drawImage(outline, 0, 0, canvas.width, canvas.height);
-
-    //     // Save the canvas as an image
-    //     download(canvas.toDataURL(), 'captured-image.png');
-    //   });
-    // }
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
   };
 
   return (
@@ -48,12 +34,6 @@ const WebcamCapture = () => {
         <img src={faceOutline} alt="Outline" />
       </div>
       <button onClick={capture} id='capture-button'><BsCamera size={30}/></button>
-      {/* {capturedImage && (
-        <div id="capture">
-          <img src={capturedImage} alt="Captured" />
-          <button onClick={saveImage}>{BsCamera}</button>
-        </div>
-      )} */}
     </div>
   );
 };
