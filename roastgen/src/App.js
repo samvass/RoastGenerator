8a9d4c464ed50848ca09461f592e9a29cecb4c59
Tara Ginsberg <tara.ginsberg@mail.mcgill.ca>
2023-10-01 11:25:06 -0400

    fixed UI

--- roastgen/src/App.js
+++ roastgen/src/App.js
@@ -1,22 +1,31 @@
-import './App.css';
-import WebcamCapture from './webcam/Webcam';
-import { useState } from 'react';
-
+import "./App.css";
+import WebcamCapture from "./webcam/Webcam";
+import { useState } from "react";
 
 function App() {
-  const [generatedText, setGeneratedText] = useState('');
+  const [generatedText, setGeneratedText] = useState("");
 
-    return (
-      <div className='app-container'>
-        <h1>Roast your friends, blame AI</h1>
-        <WebcamCapture setGeneratedText={setGeneratedText}  />
-          
-          {generatedText && <div style={{margin: '80px', fontSize: "30px", fontWeight: "500", backgroundColor: 'white', borderRadius: '20px', padding: '80px'}}>
-            {generatedText}
-          </div>}
-      </div>
-    );
-  }
+  return (
+    <div className='app-container'>
+      <h1>roast your friends, blame AI</h1>
+      <WebcamCapture setGeneratedText={setGeneratedText} />
 
+      {generatedText && (
+        <div
+          style={{
+            margin: "80px",
+            fontSize: "30px",
+            fontWeight: "500",
+            backgroundColor: "white",
+            borderRadius: "20px",
+            padding: "80px",
+          }}
+        >
+          {generatedText}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default App;
